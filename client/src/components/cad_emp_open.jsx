877a451e57cf59cdf19b7d51bb8499eefa221fa7
sverissimo <sandroverissimo@live.com>
2018-07-04 18:43:42 -0300

    Added dynamic enable / disable to Emp, RT and process forms within a object variable. Yet to link that to state, autocomplete and undo logic

--- client/src/components/cad_emp_open.jsx
+++ client/src/components/cad_emp_open.jsx
@@ -1,44 +1,70 @@
 import React from 'react';
 
-const OpenProcess = (props) =>  
-{
-    const view = props.data.openProcess;
-    if(view === true) {
-        return (
-    
-            <div style={{marginBottom: '10px'}} >
-                <fieldset className="input-field">
-                    <legend className="input-field">
-                        <strong> Dados do Empreendimento </strong>
-                    </legend>
-                    <div className="row">
-                        <div className="input-field col s3">
-                            <input type="text" className="validate" name="nomeEmpreendimento" /* onChange={props.handleChange} value={props.data.nomeRt} */ />
-                            <label className="active" htmlFor="nomeEmpreendimento">Nome do Empreendimento</label>
-                        </div>
-                        <div className="input-field col s3">
-                            <input type="number" className="validate" name="area" /* onChange={props.handleChange} value={props.data.phoneRt} */ />
-                            <label className="active" htmlFor="area">Área Total da Gleba (m²):</label>
-                        </div>
-                        <div className="input-field col s3 ">
-                            <input type="text" className="validate" name="modalidade" /* onChange={props.handleChange} value={props.data.emailRt} */ />
-                            <label className="active" htmlFor="modalidade">Modalidade:</label>
-                        </div>
-                        <div className="input-field col s3 ">
-                            <input type="text" className="validate" name="munEmpreendimento"/*  onChange={props.handleChange} value={props.data.emailRt} */ />
-                            <label className="active" htmlFor="munEmpreendimento">Município:</label>
-                        </div>
-            
-                    </div>
-            
-                </fieldset>
-                </div>
-            )
-    } 
-    else {
-        return null
+
+let enableInput;
+const voltarEmpRT = () => {
+    enableInput = {
+...enableInput, enable: "disabled"
     }
+        }
+        
+
+const OpenProcess = (props) => {
+
+    const view = props.data.openProcess;
+   
+    if (view === false) {
+        enableInput = {
+            enable: "disabled",
+            alt: "Favor preencher os dados do empreendedor e do RT",
+            autoFocus: false
+        }
+    } else {
+        enableInput = {
+            enable: "",
+            alt: "",
+            autoFocus: true
+        }
+      
+    }
+    return (
+
+        <div style={{ marginBottom: '10px' }} >
+            <fieldset className="input-field">
+                <legend className="input-field">
+                    <strong> Dados do Empreendimento </strong>
+                </legend>
+                <div className="row">
+                    <div className="input-field col s3 tooltipped" data-position="bottom" data-tooltip={enableInput.alt}>
+                        <input type="text" className="validate" name="nomeEmpreendimento" disabled={enableInput.enable} autoFocus={enableInput.autoFocus}/* onChange={props.handleChange} value={props.data.nomeRt} */ />
+                        <label className="active" htmlFor="nomeEmpreendimento">Nome do Empreendimento</label>
+                    </div>
+                    <div className="input-field col s3 tooltipped" data-position="bottom" data-tooltip={enableInput.alt}>
+                        <input type="number" className="validate" name="area" disabled={enableInput.enable} alt={enableInput.alt}/* onChange={props.handleChange} value={props.data.phoneRt} */ />
+                        <label className="active" htmlFor="area">Área Total da Gleba (m²):</label>
+                    </div>
+                    <div className="input-field col s3 tooltipped" data-position="bottom" data-tooltip={enableInput.alt}>
+                        <input type="text" className="validate" name="modalidade" disabled={enableInput.enable} alt={enableInput.alt}/* onChange={props.handleChange} value={props.data.emailRt} */ />
+                        <label className="active" htmlFor="modalidade">Modalidade:</label>
+                    </div>
+                    <div className="input-field col s3 tooltipped" data-position="bottom" data-tooltip={enableInput.alt}>
+                        <input type="text" className="validate" name="munEmpreendimento" disabled={enableInput.enable} /*  onChange={props.handleChange} value={props.data.emailRt} */ />
+                        <label className="active" htmlFor="munEmpreendimento">Município:</label>
+                    </div>
+
+                </div>
+
+            </fieldset>
+            <div>
+                <input className="btn red darken-3 left" type="submit" disabled={enableInput.enable} value="Editar dados interessado e RT" style={{ marginTop: '10px' }} onSubmit={voltarEmpRT}/>
+                <input className="btn right" type="submit" disabled={enableInput.enable} value="Cadastrar processo" style={{ marginTop: '10px' }} />
+            </div>
+        </div>
+
+    )
 }
+
+
 
 
 export default OpenProcess;
--- client/src/components/cad_emp_template.jsx
+++ client/src/components/cad_emp_template.jsx
@@ -1,69 +1,81 @@
 import React from 'react';
 
-
-
 const CadEmpTemplate = (props) => {
 
-    return (
+    const view = props.data.openProcess;
+        let enableInput;
+        if(view === true) {
+            enableInput = {
+                enable: "disabled",
+                alt: "Preencher os dados do processo ou clique em voltar"
+            }
+        } else {
+            enableInput = {
+                enable: "",
+                alt: ""
+            }
+        }
 
-        <div style={{ marginLeft: 40, marginRight: 40  }} >
+    return (
+        <div style={{ marginLeft: 40, marginRight: 40 }} >
             <div className="tab-pane fade show active" id="empreend" role="tabpanel" aria-labelledby="empreend-tab">
+            <p>Preencha os dados do interessado e RT do processo. Caso o interessado e RT não estejam cadastrados, um novo cadastro será gerado automaticamente.</p>
                 <form onSubmit={props.handleSubmit}>
                     <fieldset className="input-field">
                         <legend className="input-field">
-                            <strong> Empreendedor Responsável </strong>
+                            <strong> Interessado </strong>
                         </legend>
 
                         <div className="row">
                             <div className="input-field col s3">
-                                <input type="text" className="validate" name="nome" onChange={props.handleChange} value={props.data.nome} />
+                                <input type="text" className="validate" name="nome" onChange={props.handleChange} value={props.data.nome} disabled={enableInput.enable}/>
                                 <label className="active" htmlFor="nome">Nome</label>
                             </div>
                             <div className="input-field col s2">
-                                <input type="number" className="validate" name="cpf" onChange={props.handleChange} value={props.data.cpf} />
+                                <input type="number" className="validate" name="cpf" onChange={props.handleChange} value={props.data.cpf} disabled={enableInput.enable}/>
                                 <label className="active" htmlFor="cpf">CPF / CNPJ:</label>
                             </div>
 
                             <div className="input-field col s2">
-                                <input type="number" className="validate" name="phone" onChange={props.handleChange} value={props.data.phone} />
+                                <input type="number" className="validate" name="phone" onChange={props.handleChange} value={props.data.phone} disabled={enableInput.enable}/>
                                 <label className="active" htmlFor="phone">Telefone:</label>
                             </div>
                             <div className="input-field col">
-                                <input type="text" className="validate" name="email" onChange={props.handleChange} value={props.data.email} />
+                                <input type="text" className="validate" name="email" onChange={props.handleChange} value={props.data.email} disabled={enableInput.enable}/>
                                 <label className="active" htmlFor="email">E-mail:</label>
                             </div>
                             <div className="row">
                                 <div className="input-field col s2">
-                                    <input type="text" className="validate" name="cep" id="cep" maxLength="9" onChange={props.handleChange} onBlur={props.handleBlur} value={props.data.cep} />
+                                    <input type="text" className="validate" name="cep" id="cep" maxLength="9" onChange={props.handleChange} onBlur={props.handleBlur} value={props.data.cep} disabled={enableInput.enable}/>
                                     <label className="active" htmlFor="cep">CEP:</label>
                                 </div>
                             </div>
 
 
                             <div className="input-field col s1">
-                                <input type="text" className="validate" placeholder=" " name="numero" onChange={props.handleChange} value={props.data.numero} />
+                                <input type="text" className="validate" placeholder=" " name="numero" onChange={props.handleChange} value={props.data.numero} disabled={enableInput.enable}/>
                                 <label className="active" htmlFor="numero">Número:</label>
                             </div>
                             <div className="input-field col s2">
-                                <input type="text" className="validate" placeholder=" " name="complemento" onChange={props.handleChange} value={props.data.complemento} />
+                                <input type="text" className="validate" placeholder=" " name="complemento" onChange={props.handleChange} value={props.data.complemento} disabled={enableInput.enable}/>
                                 <label className="active" htmlFor="complemento">Complemento:</label>
                             </div>
 
 
                             <div className="input-field col s3">
-                                <input type="text" className="validate" placeholder=" " name="rua" onChange={props.handleChange} value={props.data.rua} />
+                                <input type="text" className="validate" placeholder=" " name="rua" onChange={props.handleChange} value={props.data.rua} disabled={enableInput.enable}/>
                                 <label className="active" htmlFor="rua">Rua:</label>
                             </div>
                             <div className="input-field col s2">
-                                <input type="text" className="validate" placeholder=" " name="bairro" onChange={props.handleChange} value={props.data.bairro} />
+                                <input type="text" className="validate" placeholder=" " name="bairro" onChange={props.handleChange} value={props.data.bairro} disabled={enableInput.enable}/>
                                 <label className="active" htmlFor="bairro">Bairro:</label>
                             </div>
                             <div className="input-field col s2">
-                                <input type="text" className="validate" placeholder=" " name="cidade" onChange={props.handleChange} value={props.data.cidade} />
+                                <input type="text" className="validate" placeholder=" " name="cidade" onChange={props.handleChange} value={props.data.cidade} disabled={enableInput.enable}/>
                                 <label className="active" htmlFor="cidade">Cidade:</label>
                             </div>
                             <div className="input-field col s1">
-                                <input type="text" className="validate" placeholder=" " name="uf" onChange={props.handleChange} value={props.data.uf} />
+                                <input type="text" className="validate" placeholder=" " name="uf" onChange={props.handleChange} value={props.data.uf} disabled={enableInput.enable}/>
                                 <label className="active" htmlFor="uf">Estado:</label>
                             </div>
 
@@ -77,30 +89,32 @@ const CadEmpTemplate = (props) => {
                         </legend>
                         <div className="row">
                             <div className="input-field col s4">
-                                <input type="text" className="validate" name="nomeRt" onChange={props.handleChange} value={props.data.nomeRt} />
+                                <input type="text" className="validate" name="nomeRt" onChange={props.handleChange} value={props.data.nomeRt} disabled={enableInput.enable}/>
                                 <label className="active" htmlFor="nomeRt">Nome</label>
                             </div>
                             <div className="input-field col s2">
-                                <input type="number" className="validate" name="phoneRt" onChange={props.handleChange} value={props.data.phoneRt} />
+                                <input type="number" className="validate" name="phoneRt" onChange={props.handleChange} value={props.data.phoneRt} disabled={enableInput.enable}/>
                                 <label className="active" htmlFor="phoneRt">Telefone:</label>
                             </div>
                             <div className="input-field col s4 ">
-                                <input type="text" className="validate" name="emailRt" onChange={props.handleChange} value={props.data.emailRt} />
+                                <input type="text" className="validate" name="emailRt" onChange={props.handleChange} value={props.data.emailRt} disabled={enableInput.enable}/>
                                 <label className="active" htmlFor="emailRt">E-mail:</label>
                             </div>
-                           
+
                         </div>
 
                     </fieldset>
 
-
-                        <div>
-                            {props.children}
-                        </div>
-
                     <div>
-                        <input className="btn right" type="submit" value="Salvar" style={{marginTop: '10px'}} />
+                        <input className="btn left" type="submit" value="Confirma" style={{ marginTop: '10px' }} disabled={enableInput.enable} />
                     </div>
+                    <br />
+                    <br />
+                    <div>
+                        {props.children}
+                    </div>
+
+
 
                 </form>
             </div>
