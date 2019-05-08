e24bbc837fc6da9f7b47bc7fd39a07f17cfa1644
sverissimo <sandroverissimo@live.com>
2019-05-07 21:43:13 -0300

    Added new build

---
+++ client/build/precache-manifest.5e3d17694b3ec67a8aff8df70c60e7d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7d60b5eba39e50706162",
+    "url": "/static/js/main.4778bdfd.chunk.js"
+  },
+  {
+    "revision": "889199749a2834e4e5ff",
+    "url": "/static/js/2.ca905530.chunk.js"
+  },
+  {
+    "revision": "7d60b5eba39e50706162",
+    "url": "/static/css/main.a179d136.chunk.css"
+  },
+  {
+    "revision": "889199749a2834e4e5ff",
+    "url": "/static/css/2.f02bbd8b.chunk.css"
+  },
+  {
+    "revision": "0af593dc5e56edc6c16ee1c362ceb6fd",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b685a9b57e341c25011f057be874b6f.js"
+  "/precache-manifest.5e3d17694b3ec67a8aff8df70c60e7d6.js"
 );
 
 workbox.clientsClaim();
